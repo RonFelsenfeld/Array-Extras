d110b0509d1f13952691f76e29e1f4894e302d20
Ron Felsenfeld <ronfelsenfeld@gmail.com>
2024-02-14 17:48:44 +0200

    Add ex-reduce.js

---
+++ js/ex-reduce.js
@@ -0,0 +1,122 @@
+'use strict'
+
+// Task A
+
+var gEmployees = [
+  {
+    name: 'Joe Schmoe',
+    yearsExperience: 5,
+    department: 'IT',
+  },
+  {
+    name: 'Sally Sallerson',
+    yearsExperience: 15,
+    department: 'Engineering',
+  },
+  {
+    name: 'Bill Billson',
+    yearsExperience: 5,
+    department: 'Engineering',
+  },
+  {
+    name: 'Jane Janet',
+    yearsExperience: 15,
+    department: 'Management',
+  },
+  {
+    name: 'Bob Hope',
+    yearsExperience: 9,
+    department: 'IT',
+  },
+]
+
+////////////////////////////////////////////////////
+
+console.log('******* TASK A *******')
+
+// Task 01
+
+const yearsExperienceSum = sumAllExperience()
+console.log(`yearsExperienceSum: `, yearsExperienceSum)
+
+function sumAllExperience() {
+  return gEmployees.reduce(
+    (acc, employee) => (acc += employee.yearsExperience),
+    0
+  )
+}
+
+// Task 02
+
+const experienceByDepartment = getDepartmentExperienceMap()
+console.log(`experienceByDepartment: `, experienceByDepartment)
+
+function getDepartmentExperienceMap() {
+  const departmentExperienceMap = gEmployees.reduce((map, employee) => {
+    if (!map[employee.department]) map[employee.department] = 0
+    map[employee.department] += employee.yearsExperience
+    return map
+  }, {})
+
+  return departmentExperienceMap
+}
+
+// Task 03
+
+const groupByExperience = groupEmployeesByExperience()
+console.log(`groupByExperience: `, groupByExperience)
+
+function groupEmployeesByExperience() {
+  const experienceMap = gEmployees.reduce((map, employee) => {
+    if (!map[employee.yearsExperience]) map[employee.yearsExperience] = []
+    map[employee.yearsExperience].push(employee)
+    return map
+  }, {})
+
+  return experienceMap
+}
+
+// Task 04
+
+const empCountInDepartments = countEmpByDepartment()
+console.log(`empCountInDepartments: `, empCountInDepartments)
+
+function countEmpByDepartment() {
+  const empCount = gEmployees.reduce((map, employee) => {
+    if (!map[employee.department]) map[employee.department] = 0
+    map[employee.department]++
+    return map
+  }, {})
+
+  return empCount
+}
+
+////////////////////////////////////////////////////
+
+// Task B
+console.log('******* TASK B *******')
+
+const gValues = [1, 2, 3, 1, 1, 5, 6]
+findModes(gValues)
+
+function findModes(values) {
+  const modesMap = values.reduce((map, value) => {
+    if (!map[value]) map[value] = 0
+    map[value]++
+    return map
+  }, {})
+
+  const modes = []
+  for (const mode in modesMap) {
+    modes.push([mode, modesMap[mode]])
+  }
+
+  const maxMode = modes.reduce((max, mode) => {
+    const currValue = mode[1]
+    const currMaxValue = max[1]
+    if (currValue > currMaxValue) max = mode
+    return max
+  }, modes[0])
+
+  console.log(`The max mode was ${maxMode[0]} with ${maxMode[1]} appearances`)
+}
