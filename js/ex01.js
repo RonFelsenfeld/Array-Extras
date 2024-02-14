e7483be4f0d49efb3a9964f98f4ca3c5723c4880
Ron Felsenfeld <ronfelsenfeld@gmail.com>
2024-02-14 14:08:36 +0200

    Add ex01.js

---
+++ js/ex01.js
@@ -0,0 +1,11 @@
+'use strict'
+
+var input = ['return', 'phrases', 'with one word']
+var expected = ['return', 'phrases']
+var actual = onlyOneWord(input)
+console.log('INPUT:', input, '\nEXPECTED:', expected, '\nACTUAL:', actual)
+
+function onlyOneWord(strs) {
+  const onlyOneWordStrs = strs.filter((str) => str.split(' ').length === 1)
+  return onlyOneWordStrs
+}
