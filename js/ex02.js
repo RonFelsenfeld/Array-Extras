91eff7702442750ccd62bc6b86257e13b99b1d7a
Ron Felsenfeld <ronfelsenfeld@gmail.com>
2024-02-14 14:16:18 +0200

    Add ex02.js

---
+++ js/ex02.js
@@ -0,0 +1,16 @@
+'use strict'
+
+var input = ['abc', 'xyz']
+var expected = ['cba', 'zyx']
+var actual = reverseAll(input)
+console.log('INPUT:', input, '\nEXPECTED:', expected, '\nACTUAL:', actual)
+
+input = ['ron', 'good']
+expected = ['nor', 'doog']
+actual = reverseAll(input)
+console.log('INPUT:', input, '\nEXPECTED:', expected, '\nACTUAL:', actual)
+
+function reverseAll(strs) {
+  const reverseStrs = strs.map((str) => str.split('').reverse().join(''))
+  return reverseStrs
+}
